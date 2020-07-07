aaf1e98fc54707d3d28ccba6cfd794eb9b7f6bd8
Fernando Lisazo <jflisazo@hotmail.com>
2020-07-07 19:05:59 -0300

    Confirmación inicial

---
+++ Grados/CGrados.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Grados
+{
+    class CGrados
+    {
+        private float gradosC;
+
+        public void AsignarCentigrados(float gC)
+        {
+            gradosC = gC;
+        }
+
+        public float ObtenerFahrenheit()
+        {
+            return 9F / 5F * gradosC + 32;
+        }
+
+        public float ObtenerCentigrados()
+        {
+            return gradosC;
+        }
+    }
+}
---
+++ Grados/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Grados
+{
+    class Program
+    {
+        const int limInferior = -30;
+        const int limSuperior = 100;
+        const int incremento = 6;
+
+        public static void Main(string[] args)
+        {
+            CGrados grados = new CGrados();
+            int gradosCent = limInferior;
+            float gradosfahr = 0;
+
+            while (gradosCent <= limSuperior)
+            {
+                grados.AsignarCentigrados(gradosCent);
+                gradosfahr = grados.ObtenerFahrenheit();
+                Console.WriteLine("{0, 8:d} C {1, 8:f2} F", gradosCent, gradosfahr);
+                gradosCent += incremento;
+            }
+            
+            
+        }
+    }
+}
